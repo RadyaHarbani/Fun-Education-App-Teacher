b2bdbf8245748ad1f76ed5496053ca45169e31a7
RadyaHarbani <radya.i4m@gmail.com>
2024-07-08 00:26:13 +0700

    feat [RH] : slicing edit task page

---
+++ lib/app/pages/edit-task-page/components/edit_task_page_component_one.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:fun_education_app_teacher/app/global-component/custom_radio_button.dart';
+import 'package:fun_education_app_teacher/app/pages/edit-task-page/edit_task_page_controller.dart';
+import 'package:fun_education_app_teacher/common/helper/themes.dart';
+import 'package:get/get.dart';
+
+class EditTaskPageComponentOne extends GetView<EditTaskPageController> {
+  const EditTaskPageComponentOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size mediaQuery = MediaQuery.of(context).size;
+    final double height = mediaQuery.height;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Tipe Tugas',
+          style: tsBodyLargeRegular(blackColor),
+        ),
+        SizedBox(height: height * 0.02),
+        Obx(
+          () => Column(
+            children: [
+              CustomRadioButton(
+                title: 'Dikte & Menulis',
+                value: 'Dikte & Menulis',
+                color: blueColor.withOpacity(0.05),
+                groupValue: controller.selectedType.value,
+                onChanged: (value) => controller.selectType(value),
+              ),
+              CustomRadioButton(
+                title: 'Kreasi',
+                value: 'Kreasi',
+                color: primaryColor.withOpacity(0.05),
+                groupValue: controller.selectedType.value,
+                onChanged: (value) => controller.selectType(value),
+              ),
+              CustomRadioButton(
+                title: 'Membaca',
+                value: 'Membaca',
+                color: greenColor.withOpacity(0.05),
+                groupValue: controller.selectedType.value,
+                onChanged: (value) => controller.selectType(value),
+              ),
+              CustomRadioButton(
+                title: 'Berhitung',
+                value: 'Berhitung',
+                color: warningColor.withOpacity(0.05),
+                groupValue: controller.selectedType.value,
+                onChanged: (value) => controller.selectType(value),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
