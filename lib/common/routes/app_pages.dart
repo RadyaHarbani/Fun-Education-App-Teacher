095eda3f9742c98400dd2bc8ea88284ec3cc0c1f
RadyaHarbani <radya.i4m@gmail.com>
2024-08-28 06:48:06 +0700

    fix [RH] : finishing for gladi

--- lib/common/routes/app_pages.dart
+++ lib/common/routes/app_pages.dart
@@ -56,7 +56,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN_PAGE;
+  static const INITIAL = Routes.SPLASH_SCREEN;
 
   static final routes = [
     GetPage(
